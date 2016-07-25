9d4a4a0bc758e4850bb7922c6996d3b463021431
Andrii Khanchuk <andreyblind@gmail.com>
2016-07-25 15:33:58 +0300

    implemented display logic in chat

--- public/app/index.js
+++ public/app/index.js
@@ -5,34 +5,37 @@ var app = angular.module("chatApp",['ngRoute'])
                 templateUrl: '/static/app/views/index.html'
             })
             .when('/chat',{
-                templateUrl: '/static/app/views/chat.html'
+                templateUrl: '/static/app/views/chat.html',
+                controller: 'chatController'
             })
     })
 .controller('chatController',['$scope', '$interval','$http', function($scope, $interval, $http) {
 
     $scope.newMessage = null;
     $scope.messages = [];
+    $scope.name = "";
+    $scope.displayChat = false;
 
-
+    $scope.showChat = function(){
+        if($scope.name.length > 2){
+            $scope.displayChat = true;
+        }
+    };
     var ajaxInterval = $interval(function(){
         $http.get('/messages').then(function(response){
             if (response.status == 200){
                 $scope.messages = response.data;
-                console.log(response.data);
             }
         });
-    }, 4000);
+    }, 3000);
     $scope.addNewMessage = function(){
-        console.log("send:"+$scope.newMessage);
         if($scope.newMessage) {
             $http({method: 'POST',
                 url: '/addmessage',
-                data:{name: "jphn doe", message: $scope.newMessage}}).then(function(){
+                data:{name: $scope.name, message: $scope.newMessage}}).then(function(){
                     console.log("1");
             })
         }
         $scope.newMessage = null;
     }
-
-
 }]);
