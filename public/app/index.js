3a071bc348932cb38281024878bce974fed3c39a
Andrii Khanchuk <andreyblind@gmail.com>
2016-07-25 04:15:36 +0300

    added body-parser to handle post-requests

--- public/app/index.js
+++ public/app/index.js
@@ -1,21 +1,29 @@
 var app = angular.module("chatApp",[])
 .controller('chatController',['$scope', '$interval','$http', function($scope, $interval,$http) {
-    $scope.messages =[
 
-    ];
     $scope.newMessage = null;
+    $scope.messages = [];
+
+
     var ajaxInterval = $interval(function(){
         $http.get('/messages').then(function(response){
             if (response.status == 200){
+                $scope.messages = response.data;
                 console.log(response.data);
             }
         });
-    }, 2000);
+    }, 4000);
     $scope.addNewMessage = function(){
+        console.log("send:"+$scope.newMessage);
         if($scope.newMessage) {
-            $scope.messages.push({author: 'default', text: $scope.newMessage});
-            $scope.newMessage = null;
+            $http({method: 'POST',
+                url: '/addmessage',
+                data:{name: "jphn doe", message: $scope.newMessage}}).then(function(){
+                    console.log("1");
+            })
         }
+        $scope.newMessage = null;
     }
+
 
 }]);
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 var server = app.listen(8000);
+var bodyParser = require('body-parser');
 app.use('/static', express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 var messages = [];
 
 app.get('/messages', function (req, res) {
 	res.json(messages);
 });
 
+app.post('/addmessage', function(req, res){
+	var message = {name: req.param('name'), message: req.param('message')};
+	messages.push(message);
+	console.log(req.params);
+});
+
 app.get('/', function(req, res){
 	res.sendfile('public/index.html');
-})
+});
